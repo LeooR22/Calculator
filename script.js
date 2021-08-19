b4aec7dc291e35ba302a5cfb21960d6e1fa2f4ec
leoor22 <leonelrivas.rl@gmail.com>
2021-08-19 16:58:33 -0300

    Progress in template of calculator, and scripts basic of the project

---
+++ script.js
@@ -0,0 +1,32 @@
+const add = function(a,b) {
+    return a+b   
+  };
+  
+  const subtract = function(a,b) {
+    return a-b
+  };
+  
+  const multiply = function(array) {
+    return a*b
+  };
+  
+const divide = function(a,b){
+    return a/b
+}
+
+  const operate = function(operator, a, b){
+      a = Number (a)
+      b = Number (b)
+      switch (operator){
+        case "+":
+              return add(a,b)
+        case "−":
+              return subtract(a,b)
+        case "×":
+              return multiply(a,b)
+        case "÷":
+              return divide(a,b)
+        default:
+            return null
+      }
+  }
